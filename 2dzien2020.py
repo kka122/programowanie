f8c265273b8f2e0c543d1ebce133e435ffb59ff0
kka122 <118765071+kka122@users.noreply.github.com>
2023-01-22 14:51:52 +0100

    22.01.2023

--- 2dzien2020.py
+++ 2dzien2020.py
@@ -19,6 +19,30 @@ with open("drugidzien2020.txt","r") as f:
         liczba_liter=haslo.count(litera1)
         print(liczba_liter)
         print(zakres1[-1])
-        if int(zakres1[0]) >= liczba_liter and liczba_liter <= int(zakres1[-1]):
+        if liczba_liter >= int(zakres1[0]) and liczba_liter <= int(zakres1[-1]):
+            count+=1
+print(count)
+with open("drugidzien2020.txt","r") as f:
+    wszystko=f.read().split("\n")
+    dane = []
+    for i in wszystko:
+        item=i.split()
+        dane.append(item)
+    print(dane)
+    print("to tego momentu jest g")
+    gotowe_dane=[]
+    count=0
+    jd=[]
+    for itm in dane:
+        print(itm)
+        zakres = itm[0]
+        litera = itm[1]
+        haslo = itm[2]
+        zakres1=zakres.split("-")
+        litera1=litera.replace(":","")
+        liczba_liter=haslo.count(litera1)
+        print(liczba_liter)
+        print(zakres1[0])
+        if haslo[int(zakres1[0])] == litera1 or haslo[int(zakres1[-1])] == litera1:
             count+=1
 print(count)
