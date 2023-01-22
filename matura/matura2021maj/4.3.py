bf417b3a1b8fdb5161849db5e7aadb780539edda
kka122 <118765071+kka122@users.noreply.github.com>
2023-01-23 00:30:22 +0100

    Update 4.3.py

---
+++ matura/matura2021maj/4.3.py
@@ -0,0 +1,22 @@
+with open("instrukcje.txt","r") as f:
+    linijki=f.read().split("\n")
+    print(linijki)
+    litery=[]
+count=0
+ilosc_liter={}
+for item in linijki:
+    dane=item.split(" ")
+    print(dane)
+    if dane[0]=="DOPISZ":
+        litery.append(dane[-1])
+for litera in litery:
+    value=litery.count(litera)
+    key=str(litera)
+    ilosc_liter[value]=key
+print(ilosc_liter)
+maksymalna_wartosc=max(ilosc_liter.keys())
+print(maksymalna_wartosc)
+szukana_litera=ilosc_liter[37]
+print(szukana_litera)
+
+
