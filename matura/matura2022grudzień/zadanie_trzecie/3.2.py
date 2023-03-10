b1bc8f59d7755d58b342429c90d64e2e8f6e48e3
kka122 <118765071+kka122@users.noreply.github.com>
2023-03-10 19:23:14 +0100

    Update 3.2.py

--- matura/matura2022grudzień/zadanie_trzecie/3.2.py
+++ matura/matura2022grudzień/zadanie_trzecie/3.2.py
@@ -3,8 +3,11 @@ with open("liczby_przyklad.txt","r")as f:
 liczby_pierwsze=0
 for i in wszystko:
     k=int(i)-1
-    for g in range(2,k-1):
-        if k%g==0:
-            print("liczba nie jest pierwsza")
-            liczby_pierwsze+=1
+def czy_liczba_jest_pierwsza(k):
+    for j in range(2,k):
+        if k%j==0:
+            return False
+        return 1
+if czy_liczba_jest_pierwsza(k)==1:
+    liczby_pierwsze+=1
 print(liczby_pierwsze)
