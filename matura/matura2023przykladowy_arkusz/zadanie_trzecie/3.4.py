cfa8bd55d306c76dc00daa708eddb9f46fdd44a4
kka122 <118765071+kka122@users.noreply.github.com>
2023-03-12 18:30:51 +0100

    12.02.2023
    12.03.2023

--- matura/matura2023przykladowy_arkusz/zadanie_trzecie/3.4.py
+++ matura/matura2023przykladowy_arkusz/zadanie_trzecie/3.4.py
@@ -1,6 +1,6 @@
 with open("liczby_przyklad.txt","r") as f:
     wszystko=f.read().split("\n")
-ilosc_liczb_pierwszych=0
+ilosc_par=0
 def czy_liczba_jest_pierwsza(x):
     for i in range(2,x):
         if x%i==0:
@@ -10,8 +10,19 @@ for item in wszystko:
     liczby=item.split()
     liczba_M=int(liczby[0])
     liczba_a=int(liczby[1])
-    if czy_liczba_jest_pierwsza(liczba_M)==1:
-        ilosc_liczb_pierwszych+=1
-    if czy_liczba_jest_pierwsza(liczba_a) == 1:
-        ilosc_liczb_pierwszych+=1
-print(ilosc_liczb_pierwszych)
+def czy_liczba_M_i_a_sa_wzglednie_pierwsze(x,y):
+    if x-y<0:
+        wieksza_liczba=y
+    else:
+        wieksza_liczba=x
+    for i in range(2,wieksza_liczba):
+        if x%i==0 and y%i==0:
+            return False
+    return 1
+
+if czy_liczba_jest_pierwsza(liczba_M,liczba_a)==1:
+    ilosc_par+=1
+
+print(ilosc_par)
+
+
