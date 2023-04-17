7ade1dfc8f32f76401a02273fb5a6b11799e6368
kka122 <118765071+kka122@users.noreply.github.com>
2023-04-17 18:09:22 +0200

    17.04.2023

--- advent_of_code_2016/dzien_drugi.py
+++ advent_of_code_2016/dzien_drugi.py
@@ -4,18 +4,28 @@ with open("dzien_drugi","r") as f:
 wiersze=1
 kolumny=1
 punkt_poczatkowy=keypad[wiersze][kolumny]
+haslo=[]
 for komendy in wszystko:
     for komenda in komendy:
-        if komenda=="U" and 1==wiersze:
-            wiersze-=1
-            print(keypad[wiersze][kolumny])
-        if komenda=="L" and kolumny==1:
-            kolumny-=1
-            print(keypad[wiersze][kolumny])
-        if komenda=="R" and kolumny==1:
-            kolumny+=1
-            print(keypad[wiersze][kolumny])
-        if komenda=="D" and wiersze ==1:
-            wiersze+=1
-            print(keypad[wiersze][kolumny])
-    print(keypad[wiersze][kolumny])
+        if komenda=="U":
+            if wiersze == 0:
+                wiersze=wiersze
+            if wiersze==1 or wiersze ==2:
+                wiersze-=1
+        elif komenda=="L":
+            if  kolumny ==0:
+                kolumny=kolumny
+            if kolumny ==1 or wiersze==2:
+                kolumny-=1
+        elif komenda=="R":
+            if kolumny ==2:
+                kolumny=kolumny
+            if kolumny==0 or kolumny==1:
+                kolumny+=1
+        elif komenda=="D":
+            if wiersze ==2:
+                wiersze = wiersze
+            if wiersze==0 or wiersze==1 :
+                wiersze+=1
+        print(keypad[wiersze][kolumny])
+print(haslo)
