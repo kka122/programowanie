1ca25a0d0434b4e6a61a8c2ef06fcea19311be81
kka122 <118765071+kka122@users.noreply.github.com>
2023-05-03 00:02:17 +0200

    03.05.2023

--- advent_of_code_2022/dzien_trzeci.py
+++ advent_of_code_2022/dzien_trzeci.py
@@ -1,11 +1,27 @@
 with open("dzien_trzeci","r")as f:
     plecaki=f.read().split("\n")
 plecak_podzielony=[]
+podzielony_przedzial=[]
+litery=[]
+wlasciwe_litery=[]
 for plecak in plecaki:
     p=int(len(plecak)/2)
-    przedzial1=[]
-    for i in range(p):
-        przedzial1.append(plecak[p])
-print(przedzial1)
+    przedzial1=plecak[:p]
+    przedzial2=plecak[p:]
+    for litera in przedzial1:
+        for litera1 in przedzial2:
+            if litera==litera1:
+                litery.append(litera1)
+p=0
+dobre_litery=set(litery)
+for i in dobre_litery:
+
+    if i ==i.upper():
+        p+=ord(i)-65+27
+
+    elif i ==i.lower():
+        p+=ord(i)-64-32
+print(p)
+
 
 
---
+++ matura/informator_maturalny/zadanie_7/7.2.py
@@ -0,0 +1,5 @@
+
+print(ord("J"))
+print(74+15-26)
+print(chr(89))
+
