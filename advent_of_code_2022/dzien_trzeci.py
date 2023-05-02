bae33aba16954b83f278d598f2ceb5b757789517
kka122 <118765071+kka122@users.noreply.github.com>
2023-05-03 00:30:24 +0200

    Update dzien_trzeci.py

--- advent_of_code_2022/dzien_trzeci.py
+++ advent_of_code_2022/dzien_trzeci.py
@@ -1,27 +1,26 @@
 with open("dzien_trzeci","r")as f:
     plecaki=f.read().split("\n")
-plecak_podzielony=[]
-podzielony_przedzial=[]
 litery=[]
-wlasciwe_litery=[]
+
 for plecak in plecaki:
     p=int(len(plecak)/2)
     przedzial1=plecak[:p]
     przedzial2=plecak[p:]
+    dobre_litery = []
     for litera in przedzial1:
         for litera1 in przedzial2:
             if litera==litera1:
-                litery.append(litera1)
-p=0
-dobre_litery=set(litery)
-for i in dobre_litery:
-
-    if i ==i.upper():
-        p+=ord(i)-65+27
-
-    elif i ==i.lower():
-        p+=ord(i)-64-32
-print(p)
+                dobre_litery.append(litera1)
+    litery.append(set(dobre_litery))
+print(litery)
+x=0
+for i in litery:
+    for k in i:
+        if k ==k.upper():
+            x+=ord(k)-65+27
+        elif k ==k.lower():
+            x+=ord(k)-64-32
+print(x)
 
 
 
