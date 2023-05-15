46f810f22927556db8c654870777a1a891f187a0
kka122 <118765071+kka122@users.noreply.github.com>
2023-05-15 21:41:42 +0200

    Update 8.1.py

--- matura/informator_maturalny/zadanie_8/8.1.py
+++ matura/informator_maturalny/zadanie_8/8.1.py
@@ -1,7 +1,7 @@
 
 
 
-def input():
+def wczytanie_pliku():
     with open("dane8.txt","r") as f:
         wszystko=f.read().split("\n")
         return wszystko
@@ -9,22 +9,21 @@ def input():
 
 def sprawdzanie_luk(terazniejsza,poprzednia):
     if (int(terazniejsza)+int(poprzednia))%2==0:
-        return 1
+        return True
     elif (int(terazniejsza)+int(poprzednia))%2==1:
-        return 2
+        return False
 
 
 
 def main():
-    ciag=input()
+    ciag=wczytanie_pliku()
     liczba_luk_parzystych=0
     liczba_luk_nieparzystych=0
-    for i in range(0,2024):
-        if i <=2021:
-            if sprawdzanie_luk(ciag[i],ciag[i+1])==1:
-                liczba_luk_parzystych+=1
-            elif sprawdzanie_luk(ciag[i],ciag[i+1])==2:
-                liczba_luk_nieparzystych+=1
+    for i in range(0,2022):
+        if sprawdzanie_luk(ciag[i],ciag[i+1])==True:
+            liczba_luk_parzystych+=1
+        elif sprawdzanie_luk(ciag[i],ciag[i+1])==False:
+            liczba_luk_nieparzystych+=1
     print(liczba_luk_parzystych,liczba_luk_nieparzystych)
 
 
